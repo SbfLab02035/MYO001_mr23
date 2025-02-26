d51e72b44643770d9c2f91bbb8fea97c62a714ad
SBF-LAB02-035 <sbf-lab02-035@istinye.edu.tr>
2025-02-26 12:13:18 +0300

    example 3

--- MYO001_mr23/Program.cs
+++ MYO001_mr23/Program.cs
@@ -11,9 +11,18 @@
             int a = 5;
             int alan = a * a; 
             int cevre = 4 * a;
-            Console.WriteLine($"alan: {0}" + 
-            " cevre: {1}", alan, cevre);
+            Console.WriteLine("alan: {0}" + " cevre: {1}", alan, cevre);
 
+            double fiyat, kar, satisFiyat;
+            Console.Write("Fiyati giriniz: ");
+            fiyat = double.Parse(Console.ReadLine());
+
+            Console.Write("kari giriniz: ");
+            kar = double.Parse(Console.ReadLine());
+
+            satisFiyat = fiyat + kar;
+            Console.WriteLine("sastiş fiyati{0}", satisFiyat);
+            Console.ReadKey();
         }
     }
 }
