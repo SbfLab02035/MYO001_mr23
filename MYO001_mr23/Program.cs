0f574f2e5ec1c6b8c0fcbc0d880aef1c0f97b4f6
SBF-LAB02-035 <sbf-lab02-035@istinye.edu.tr>
2025-02-26 12:34:16 +0300

    example4

--- MYO001_mr23/Program.cs
+++ MYO001_mr23/Program.cs
@@ -13,16 +13,27 @@
             int cevre = 4 * a;
             Console.WriteLine("alan: {0}" + " cevre: {1}", alan, cevre);
 
-            double fiyat, kar, satisFiyat;
+            double fiyat, kar, satisFiyat, kdv, hamkdv;
             Console.Write("Fiyati giriniz: ");
             fiyat = double.Parse(Console.ReadLine());
 
             Console.Write("kari giriniz: ");
             kar = double.Parse(Console.ReadLine());
 
+
+            Console.WriteLine("kdv oranı giriniz: ");
+            hamkdv = Convert.ToInt32(Console.ReadLine());
+
+            hamkdv = hamkdv / 100;
             satisFiyat = fiyat + kar;
-            Console.WriteLine("sastiş fiyati{0}", satisFiyat);
+            kdv = satisFiyat + (satisFiyat * hamkdv);
+
+
+            Console.WriteLine("satis fiyati{0}", satisFiyat);
+            Console.WriteLine("kdv dahil satis fiyati{0}", kdv);
             Console.ReadKey();
+
+
         }
     }
 }
