c11a5d294bdaea6ead686466808841cf9560ef3c
SBF-LAB02-035 <SBF-LAB02-035@SBF-LAB02-035>
2025-02-26 12:01:55 +0300

    Proje dosyası ekle.

---
+++ MYO001_mr23/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace MYO001_mr23
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.Write("Hello, World!");
+            Console.Write("Hello, World!");
+
+            int a = 5;
+            int alan = a * a; 
+            int cevre = 4 * a;
+            Console.WriteLine($"alan: {0}" + 
+            " cevre: {1}", alan, cevre);
+
+        }
+    }
+}
