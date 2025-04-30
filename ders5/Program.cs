1ff3ff5e2d5382d8e6fc66496df25adc6e3884cd
SBF-LAB02-035 <sbf-lab02-035@istinye.edu.tr>
2025-04-30 13:45:15 +0300

    dögüler ve zilier

---
+++ ders5/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Runtime.InteropServices;
+
+namespace ders5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Random rnd = new Random();
+
+            //for (int i = 0; i < 2;)
+            //{
+            //    Console.Write(rnd.Next(0, 2));
+            //    Console.ForegroundColor = ConsoleColor.Green;
+            //    // System.Threading.Thread.Sleep(1);
+            //}
+
+            int x = 10;
+            for (int i = 0; i <= x; i++)
+            {
+                Console.WriteLine(i);
+                int j = i;
+
+                for (int k = x; k >= 0; k--)
+                {
+                    Console.WriteLine(k);
+                }
+
+                i = j;
+            }
+
+        }
+    }
+}
---
+++ ders6/Program.cs
@@ -0,0 +1,70 @@
+﻿namespace ders6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // İç İçe döngüler
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    for (int j = 10; j > 0; j--)
+            //    {
+            //        Console.Write("*");
+
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            // diziler
+            //double[] a = { 55, 60, 70, 40, 20, 60};
+            //int gecen = 0;
+            //for (int i = 0;i < a.Length; i++) if(a[i] >= 50) gecen++;
+            //Console.WriteLine($"gecen sayısı: {gecen}");
+            //Console.WriteLine($"barasi orani: {gecen*100/(a.Length)}");
+
+            // ornek1
+            //Console.WriteLine("ogrenci sayısını giriniz");
+            //int ogr = int.Parse(Console.ReadLine());
+            //int basari = 0;
+            //double[] liste = new double[ogr];
+
+            //Console.WriteLine("\notları giriniz");
+            //for (int i = 0; i < ogr; i++)
+            //{
+            //    liste[i] = int.Parse(Console.ReadLine());
+            //    if(liste[i] >= 50) basari++;
+            //}
+
+            //Console.WriteLine("\nGirilen notlar");
+            //for (int i = 0; i < ogr; i++)
+            //{
+            //    Console.WriteLine($"{i + 1}. sayı: {liste[i]}");
+            //}
+
+            //Console.WriteLine($"\nBaşarı yüzdesi: {100*basari/(ogr)}");
+
+            //Console.WriteLine($"\nSilme işlemi sonrası");
+
+            //Array.Clear(liste, 0, 3);
+
+            //for (int i = 0; i < ogr; i++)
+            //{
+            //    Console.WriteLine($"{i + 1}. sayı: {liste[i]}");
+            //}
+
+            // ornek2
+            int[] liste = { 10, 20, 30, 40, 50};
+            int n =liste.Length;
+            int[] b = new int[n];
+            for (int i = 0; i < n; i++) 
+                Console.WriteLine($"{liste[i]} ");
+            for (int i = 0; i < n; i++)
+                b[i] = liste[n-i-1];
+            Console.WriteLine();
+            for (int i = 0; i < n; i++)
+                Console.WriteLine($"{b[i]} ");
+
+        }
+    }
+}
